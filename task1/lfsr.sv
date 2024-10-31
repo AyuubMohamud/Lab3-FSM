d20c4d920ed552ce0639c07ca41367d3c81c02c9
Ayuub Mohamud <ayuub.mohamud@outlook.com>
2024-10-31 10:14:18 +0000

    finished task3 at home with Vbuddy

--- task1/lfsr.sv
+++ task1/lfsr.sv
@@ -4,5 +4,15 @@ module lfsr(
     input   logic       en,
     output  logic [3:0] data_out
 );
-
+    initial data_out = 4'b0001;
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            data_out <= 4'b0001;
+        end else if (en) begin
+            data_out[0] <= data_out[3]^data_out[2];
+            data_out[1] <= data_out[0];
+            data_out[2] <= data_out[1];
+            data_out[3] <= data_out[2];
+        end
+    end
 endmodule
--- task1/lfsr_7.sv
+++ task1/lfsr_7.sv
@@ -4,5 +4,18 @@ module lfsr_7 (
     input   logic       en,
     output  logic [6:0] data_out
 );
-
+    initial data_out = 7'b0000001;
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            data_out <= 7'b0000001;
+        end else if (en) begin
+            data_out[0] <= data_out[2]^data_out[6];
+            data_out[1] <= data_out[0];
+            data_out[2] <= data_out[1];
+            data_out[3] <= data_out[2];
+            data_out[4] <= data_out[3];
+            data_out[5] <= data_out[4];
+            data_out[6] <= data_out[5];
+        end
+    end
 endmodule
